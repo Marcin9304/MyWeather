85833b18e2836ae3d153c520fd993eab45485d96
Noh-All <kmuszynski15@gmail.com>
2026-01-02 11:39:41 +0100

    Zjazd 1: implementacja interfejsu i logowania biemetrucznego_2

--- app/src/main/java/com/example/myweather/MainActivity.java
+++ app/src/main/java/com/example/myweather/MainActivity.java
@@ -23,3 +23,36 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         tvStatus = findViewById(R.id.tvStatus);
         btnLogin = findViewById(R.id.btnLogin);
+        // Konfiguracja biometrii
+        executor = ContextCompat.getMainExecutor(this);
+        biometricPrompt = new BiometricPrompt(MainActivity.this, executor,
+                new BiometricPrompt.AuthenticationCallback() {
+                    @Override
+                    public void onAuthenticationSucceeded(@NonNull BiometricPrompt.AuthenticationResult result) {
+                        super.onAuthenticationSucceeded(result);
+                        tvStatus.setText("Zalogowano pomyślnie!\nSystem gotowy.");
+                        tvStatus.setTextColor(0xFF2E7D32); // Zielony kolor
+
+                        Toast.makeText(getApplicationContext(), "Sukces!",
+                                Toast.LENGTH_SHORT).show();
+                    }
+
+                    @Override
+                    public void onAuthenticationError(int errorCode, @NonNull CharSequence errString) {
+                        super.onAuthenticationError(errorCode, errString);
+                        tvStatus.setText("Błąd: " + errString);
+                    }
+                });
+
+        promptInfo = new BiometricPrompt.PromptInfo.Builder()
+                .setTitle("MyWeather Security")
+                .setSubtitle("Przyłóż palec")
+                .setNegativeButtonText("Anuluj")
+                .build();
+
+        btnLogin.setOnClickListener(v -> biometricPrompt.authenticate(promptInfo));
+
+        // Start automatyczny przy uruchomieniu
+        biometricPrompt.authenticate(promptInfo);
+    }
+}

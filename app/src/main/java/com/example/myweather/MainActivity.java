9be0cccb6edd1860b363b0a4a4745acf3cf597e3
Marcin9304 <marcin.niemiec93@gmail.com>
2026-01-02 11:21:04 +0100

    Logika biometri (czesc)

--- app/src/main/java/com/example/myweather/MainActivity.java
+++ app/src/main/java/com/example/myweather/MainActivity.java
@@ -1,24 +1,25 @@
 package com.example.myweather;
 
 import android.os.Bundle;
-
-import androidx.activity.EdgeToEdge;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
+import androidx.biometric.BiometricPrompt;
+import androidx.core.content.ContextCompat;
+import java.util.concurrent.Executor;
 
 public class MainActivity extends AppCompatActivity {
+    private TextView tvStatus;
+    private Button btnLogin;
+    private Executor executor;
+    private BiometricPrompt biometricPrompt;
+    private BiometricPrompt.PromptInfo promptInfo;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
-    }
-}
+        tvStatus = findViewById(R.id.tvStatus);
+        btnLogin = findViewById(R.id.btnLogin);
